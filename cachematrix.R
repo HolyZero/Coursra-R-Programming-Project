919241a706c03ceefd51533f9d9d6bc1ef63993f
Ziyue Jin <z3jin@ucsd.edu>
2015-06-19 23:42:34 -0700

    Coursera Week3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the inverse of a matrix to save run time.
 
-## Write a short comment describing this function
-
+## Creates a special "matrix" which has four functions. 
+## Set the value of mx, get the value of mx, set the inv
+## of mx, and get the inv of mx.
 makeCacheMatrix <- function(x = matrix()) {
-
+    m  <-  NULL
+    set  <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv  <- function(inv) m <<- inv
+    getinv  <- function() m
+    list(set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## Computes the inverse of the special "matrix" returned by
+## makeCacheMatrix above. If inverse has already been calculated
+## then cacheSolve should retrieve the inverse from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached inverse")
+        return(m)
+    }
+    mtx <- x$get()
+    m  <- solve(mtx,...)
+    x$setinv(m)
+    m
 }
